2a2ff673a9d7a9c228d05c4db2b1a94d3cdd3d53
@melkijbes <peppbaggio@yandex.ru>
2025-10-28 23:23:46 +0300

    Очищено

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,7 @@
-import data.StringTransformator;
-
 public class Main {
 
     public static void main(String[] args) {
 
-        String[] stringArray = {"привет", "ПОКА", "гипербола"};
-
-        StringTransformator newString = new StringTransformator();
-
-        System.out.println(newString.stringToUpperCase(stringArray[0]));
-
-        System.out.println(newString.stringToLowerCase(stringArray[1]));
-
-        System.out.println(newString.stringReplaceLetterAndAddSpaceStringBuilder(
-                stringArray[2], "О", " "
-        ));
-
-        System.out.println(newString.stringReplaceLetterAndAddSpaceStringMethod(
-                stringArray[2], "О", " "
-        ));
 
     }
 
