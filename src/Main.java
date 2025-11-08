579473e3379dbbe266df2ecf891e507726f1145b
@melkijbes <peppbaggio@yandex.ru>
2025-11-09 00:16:55 +0300

    Очищено

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,8 @@
-import data.Guitar;
-import data.Piano;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Guitar guitar = new Guitar("гитара \"Корт\"");
-        Piano piano = new Piano("рояль \"Стенвей\"");
-        guitar.play();
-        piano.play();
 
     }
 
--- src/data/Playable.java
+++
@@ -1,6 +0,0 @@
-package data;
-
-public interface Playable {
-
-    void  play();
-}
